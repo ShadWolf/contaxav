93b03ddd83a06bf6eae4db582f64a61fdbe51eb4
Alphe Salas <asalas@tech-group.cl>
2018-05-04 18:00:31 -0300

    thymeleaf webjars not working ...

---
+++ src/main/java/org/asalas/Application.java
@@ -0,0 +1,21 @@
+package org.asalas;
+
+import com.mongodb.MongoClient;
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
